3c40b08928125b67c16843db89341b826e9cbc1e
Marco Ruiz <marco.ruiz.di@gmail.com>
2024-07-17 04:36:57 -0400

    estructura html + modal + tooltip + alert

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,35 @@
-import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import "../../node_modules/jquery/dist/jquery.js";
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
 
+
+
+
+// Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+
+// Alert function
+function alert1() {
+    // Create the alert HTML
+    const alertHtml = `
+      <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>¡Éxito!</strong> Tu mensaje ha sido enviado correctamente.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+    `;
   
+    // Append the alert to the alert container
+    $("#alertContainer").append(alertHtml);
+  
+    // Optionally, set a timeout to automatically close the alert after a few seconds
+    setTimeout(() => {
+      $("#alertContainer .alert").remove(); // Remove the alert from the DOM
+    }, 3000); // 3000 milliseconds (3 seconds)
+  }
+  
+  // Event listener for the button
+  $("#enviarBtn").click(function() {
+    alert1();
+  });
