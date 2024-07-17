178ab7552d512b42bb3e826118684b793cb88a21
Marco Ruiz <marco.ruiz.di@gmail.com>
2024-07-17 14:15:18 -0400

    Background del navbar, smooth scrolling navbar, modal del carrusel, carrusel caption

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,26 @@ import "../../node_modules/jquery/dist/jquery.js";
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
 
+// fondo para el navbar
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-bg');
+    } else {
+        navbar.classList.remove('navbar-bg');
+    }
+});
 
+  // Smooth scrolling para los links del navbar //
+  $('.navbar-nav .nav-link').on('click', function(event) { //detecta clicks en el nav
+    var target = $(this).attr('href'); // obtiene el href del link
+    if ($(target).length) { // si el target existe
+      event.preventDefault(); // evita el comportamiento por defecto
+      $('html, body').animate({ // anima el scroll hacia el target
+        scrollTop: $(target).offset().top -90 //define hacia donde va el scroll
+      }, 100); // velocidad en ms de la animación
+    }
+  });
 
 
 // Tooltip
@@ -12,24 +31,24 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 
 // Alert function
 function alert1() {
-    // Create the alert HTML
+    // Código html que se muestra en la alerta
     const alertHtml = `
       <div class="alert alert-success alert-dismissible fade show" role="alert">
-        <strong>¡Éxito!</strong> Tu mensaje ha sido enviado correctamente.
+        <strong>¡Muy bien!</strong> Tu mensaje ha sido enviado correctamente, revisa tu bandeja de entrada para revisar nuestra respuesta.
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
     `;
   
-    // Append the alert to the alert container
+    // Append (adjunta) el código al div.alertcontainer
     $("#alertContainer").append(alertHtml);
   
-    // Optionally, set a timeout to automatically close the alert after a few seconds
+    // Después de 5 segundos el mensaje desaparece
     setTimeout(() => {
       $("#alertContainer .alert").remove(); // Remove the alert from the DOM
-    }, 3000); // 3000 milliseconds (3 seconds)
+    }, 5000); // 5000 milliseconds (5 seconds)
   }
   
-  // Event listener for the button
+  // Click en el botón (como es type=button en vez de submit tuve que cambiarlo)
   $("#enviarBtn").click(function() {
     alert1();
   });
